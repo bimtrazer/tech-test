e0bcb15c8bc03ea6532edc3a91a761a47066d53f
valen07dido <valendido69@gmail.com>
2024-08-14 08:56:12 -0300

    funcionalidad de update

--- client/src/components/BlockCard/BlockCard.jsx
+++ client/src/components/BlockCard/BlockCard.jsx
@@ -1,12 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
+import EditBlockForm from "../EditBlockForm/EditBlockForm";
 
-const BlockCard = ({block}) => {
+const BlockCard = ({ block }) => {
+  const [isEditing, setIsEditing] = useState(false);
+
+  const handleEditClick = () => {
+    setIsEditing(true);
+  };
+
+  const handleClose = () => {
+    setIsEditing(false);
+  };
+
   return (
     <div>
+      <button onClick={handleEditClick} title="Editar Bloque">
+        🖍
+      </button>
       <h3>{block.description}</h3>
       <p>Start Date: {block.startDate}</p>
       <p>End Date: {block.endDate}</p>
       <p>Progress: {block.progress}%</p>
+
+      {isEditing && <EditBlockForm block={block} onClose={handleClose} />}
     </div>
   );
 };
---
+++ client/src/components/EditBlockForm/EditBlockForm.jsx
@@ -0,0 +1,114 @@
+import React, { useState, useEffect } from "react";
+import { updateBlock } from "../../services/api";
+import Swal from "sweetalert2";
+import styles from "./EditBlockForm.module.css";
+const EditBlockForm = ({ block, onClose }) => {
+  const [editedBlock, setEditedBlock] = useState(block);
+  const [token, setToken] = useState("token");
+
+  useEffect(() => {
+    setEditedBlock(block);
+  }, [block]);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const { startDate, endDate } = editedBlock;
+
+    if (new Date(startDate) >= new Date(endDate)) {
+      Swal.fire({
+        title: "Error!",
+        text: "La fecha de inicio debe ser anterior a la fecha de culminación",
+        icon: "error",
+        confirmButtonText: "Entendido",
+      });
+      return;
+    }
+
+    try {
+      await updateBlock(block.id, editedBlock, token);
+      Swal.fire({
+        title: "Éxito!",
+        text: "Bloque actualizado correctamente",
+        icon: "success",
+        confirmButtonText: "Cool",
+      });
+      onClose();
+    } catch (error) {
+      Swal.fire({
+        title: "Error!",
+        text: "No tenemos conexión :(",
+        icon: "error",
+        confirmButtonText: "Entendido",
+      });
+    }
+  };
+
+  const onChange = (e) => {
+    setEditedBlock({
+      ...editedBlock,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  return (
+    <div className={styles.popup}>
+      <div>
+        <button title="Eliminar bloque">🗑</button>
+        <button type="button" title="Cerrar" onClick={onClose}>
+          X
+        </button>
+      </div>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Description</label>
+          <input
+            type="text"
+            value={editedBlock.description}
+            onChange={(e) => onChange(e)}
+            maxLength="40"
+            required
+            name="description"
+            readOnly
+            className={styles.description}
+          />
+        </div>
+        <div>
+          <label>Start Date</label>
+          <input
+            type="date"
+            value={editedBlock.startDate}
+            onChange={(e) => onChange(e)}
+            required
+            name="startDate"
+          />
+        </div>
+        <div>
+          <label>End Date</label>
+          <input
+            type="date"
+            value={editedBlock.endDate}
+            onChange={(e) => onChange(e)}
+            required
+            name="endDate"
+          />
+        </div>
+        <div>
+          <label>Progress</label>
+          <input
+            type="number"
+            value={editedBlock.progress}
+            onChange={(e) => onChange(e)}
+            min="0"
+            max="100"
+            required
+            name="progress"
+          />
+        </div>
+        <button type="submit">Update Block</button>
+      </form>
+    </div>
+  );
+};
+
+export default EditBlockForm;
