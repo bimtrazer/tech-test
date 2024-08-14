0df5325f07e4cb201183e585d25b643f74a40d94
Noelia-Ruiz <ruiz.noeliaromina@gmail.com>
2024-08-13 22:23:05 -0300

    Se crea conexiòn con base de datos en mongodb atlas, se instalan dependencias -mongoose- y -.env-

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config();
 
 const app = express ();
 const port = process.env.PORT || 3000;
@@ -7,6 +9,13 @@ const port = process.env.PORT || 3000;
 app.get('/', (req, res) => {
     res.send('welcome to my project')
 })
+
+//se crea coneccion con mongo DB Atlas
+mongoose
+    .connect(process.env.MONGODB_URL)
+    .then(() => console.log('Conectado a la base de datos en MongoDB Atlas'))
+    .catch((error) => console.error(error)
+    )
 
 app.listen(port, () => console.log('servidor corriendo en puerto', port));
 
