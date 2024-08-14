48ee1baff14cc3967ae907c01f9cc46936a90395
valentin di domenica <valendido69@gmail.com>
2024-08-13 22:50:44 -0300

    componente list y card

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,13 +1,19 @@
 import axios from "axios";
 const API_URL = "http://localhost:3000/blocks";
-export const getBLocks = async (token) => {
-  const { data } = await axios.get(API_URL, {
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-  });
+import { blockTest } from "../utils/data";
 
-  return data;
+export const getBLocks = async (token) => {
+  try {
+    const { data } = await axios.get(API_URL, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    return data;
+  } catch (error) {
+    console.error("Error fetching blocks:", error);
+    return blockTest;
+  }
 };
 
 export const createBlock = async (block, token) => {
